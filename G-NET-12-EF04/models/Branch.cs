504f412b934b9c30df2aa5d69fa7a2e70b6c5dc5
marco <marco@DESKTOP-GCUIOA1>
2026-05-06 00:21:40 +0300

    Branch

---
+++ G-NET-12-EF04/models/Branch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF04.models
+{
+          public class Branch
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string BranchCode { get; set; }
+
+            public string Address { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public int ManagerId { get; set; }
+
+            public Manager Manager { get; set; }
+
+            public ICollection<Account> Accounts { get; set; }
+                = new HashSet<Account>();
+        }
+    }
