1a7ec56582abc36a76d7fad4f20dc512e309f73c
marco <marco@DESKTOP-GCUIOA1>
2026-05-06 00:33:39 +0300

    Account

---
+++ G-NET-12-EF04/models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace G_NET_12_EF04.models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string AccountType { get; set; }
+
+        public DateTime OpeningDate { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public int BranchId { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public ICollection<CustomerAccount> CustomerAccounts { get; set; }
+            = new HashSet<CustomerAccount>();
+
+        public ICollection<Transaction> Transactions { get; set; }
+            = new HashSet<Transaction>();
+    }
+}
