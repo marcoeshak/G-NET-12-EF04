79ea4e719dd76f6ef0f64d7793348a45c8dc6ae3
marco <marco@DESKTOP-GCUIOA1>
2026-05-06 00:32:49 +0300

    Customer

---
+++ G-NET-12-EF04/models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF04.models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string CustomerType { get; set; }
+
+        public ICollection<CustomerAccount> CustomerAccounts { get; set; }
+            = new HashSet<CustomerAccount>();
+    }
+}
