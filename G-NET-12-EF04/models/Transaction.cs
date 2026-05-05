6c4c18a0373569a669ca14268125f5da2cf78e82
marco <marco@DESKTOP-GCUIOA1>
2026-05-06 00:35:24 +0300

    Transaction

---
+++ G-NET-12-EF04/models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF04.models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string TransactionType { get; set; }
+
+        public string Description { get; set; }
+
+        public int AccountId { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
