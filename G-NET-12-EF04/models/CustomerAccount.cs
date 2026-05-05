00d08d95e7c1e871abfcfaa989ba7c5946311ff4
marco <marco@DESKTOP-GCUIOA1>
2026-05-06 00:34:37 +0300

    CustomerAccount

---
+++ G-NET-12-EF04/models/CustomerAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF04.models
+{
+    public class CustomerAccount
+    {
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public int AccountId { get; set; }
+
+        public Account Account { get; set; }
+
+        public DateTime OwnershipStartDate { get; set; }
+
+        public string OwnershipType { get; set; }
+
+        public bool AccountStatus { get; set; }
+    }
+}
